59fca7dbbcd228d0e93ddacf559cfab4ac9fdd95
Angelo Zero <angelo.akm@gmail.com>
2024-03-24 13:44:41 -0300

    adicionando filtro por departamento na consulta de usuario

--- app.py
+++ app.py
@@ -1,21 +1,24 @@
 from flask import Flask, jsonify, request
 from flask_marshmallow import Marshmallow
-from models import User, Departament, db
+from models import User, Department, db
 from datetime import datetime
+from dotenv import load_dotenv
 
+import os
+
+load_dotenv()
+SQLALCHEMY_DATABASE_URI = os.getenv("SQLALCHEMY_DATABASE_URI")
 
 app = Flask(__name__)
 
-app.config["SQLALCHEMY_DATABASE_URI"] = (
-    "postgresql://famufcyv:FfNdYCVSXUxGr9g-i0IXhKGHPPsCCf-T@raja.db.elephantsql.com/famufcyv"
-)
+app.config["SQLALCHEMY_DATABASE_URI"] = SQLALCHEMY_DATABASE_URI
 
 
 db.init_app(app)
 with app.app_context():
     db.drop_all()
     db.create_all()
-    Departament.insert_initial_values()
+    Department.insert_initial_values()
     User.insert_initial_values()
 
 ma = Marshmallow(app)
@@ -23,25 +26,31 @@ ma = Marshmallow(app)
 
 class UserSchema(ma.Schema):
     class Meta:
-        fields = ("id", "name", "departament")
+        fields = ("id", "name", "department")
 
 
 users_schema = UserSchema(many=True)
 
 
-class DepartamentSchema(ma.Schema):
+class DepartmentSchema(ma.Schema):
     class Meta:
         fields = ("id", "name")
 
 
-departament_schema = DepartamentSchema(many=True)
+department_schema = DepartmentSchema(many=True)
 
 
 @app.route("/usuarios", methods=["GET"])
 def list_users():
     paginated_users = User.get_paginated_users(
-        request.args.get("pagina", 1, type=int), request.args.get("total", 5, type=int)
+        request.args.get("pagina", 1, type=int),
+        request.args.get("total", 5, type=int),
+        request.args.get("departamento", None, type=str),
     )
+
+    if paginated_users is None:
+        return jsonify({"error": "Não ha registros de usuários"}), 404
+
     return paginated_users
 
 
@@ -55,19 +64,19 @@ def user_detail(id):
 def create_user():
     data = request.json
 
-    # Verifica se o campo de Departamento esta presente
-    if "name" not in data or "departament_id" not in data:
-        return jsonify({"error": "Não foi enviado a informação de departamento"}), 400
+    # Verifica se o campo de Departmento esta presente
+    if "name" not in data or "department_id" not in data:
+        return jsonify({"error": "Não foi enviado a informação de departmento"}), 400
 
-    # Verifica se o ID do departamento é válido
-    if Departament.query.filter_by(id=data["departament_id"]).first() is None:
-        return jsonify({"error": "Departamento inválido"}), 400
+    # Verifica se o ID do departmento é válido
+    if Department.query.filter_by(id=data["department_id"]).first() is None:
+        return jsonify({"error": "Departmento inválido"}), 400
 
     new_user = User(
         name=data["name"],
         second_name=data["second_name"],
         email=data["email"],
-        departament_id=data["departament_id"],
+        department_id=data["department_id"],
         date_time_creation=datetime.now(),
         date_time_updated=None,
     )
@@ -107,51 +116,49 @@ def delete_user(id):
         return jsonify({"error": "Usuário não foi encontrado"}), 404
 
 
-@app.route("/departamentos", methods=["GET"])
-def list_departament():
-    all_departaments = Departament.query.all()
-    results = departament_schema.dump(all_departaments)
+@app.route("/departmentos", methods=["GET"])
+def list_department():
+    all_departments = Department.query.all()
+    results = department_schema.dump(all_departments)
     return jsonify(results)
 
 
-@app.route("/departamento/<id>", methods=["GET"])
-def departament_detail(id):
-    departament = Departament.query.get(id)
+@app.route("/departmento/<id>", methods=["GET"])
+def department_detail(id):
+    department = Department.query.get(id)
     return {
-        "id": departament.id,
-        "departament": departament.name,
+        "id": department.id,
+        "department": department.name,
     }
 
 
-@app.route("/departamento", methods=["POST"])
-def create_departament():
+@app.route("/departmento", methods=["POST"])
+def create_department():
     data = request.json
 
     # Verifica se todos os campos necessários estão presentes
     if "name" not in data:
         return (
             jsonify(
-                {"error": "O campo nome para o cadastro de Departamento é obrigatório"}
+                {"error": "O campo nome para o cadastro de Departmento é obrigatório"}
             ),
             400,
         )
 
-    # Verifica se já existe um departamento com o mesmo nome
-    existing_departament = Departament.query.filter_by(
-        name=data["name"].upper()
-    ).first()
-    if existing_departament:
-        return jsonify({"error": "Departamento já existe"}), 409  # 409 - Conflict
+    # Verifica se já existe um departmento com o mesmo nome
+    existing_department = Department.query.filter_by(name=data["name"].upper()).first()
+    if existing_department:
+        return jsonify({"error": "Departmento já existe"}), 409
 
-    new_departament = Departament(name=data["name"].upper())
+    new_department = Department(name=data["name"].upper())
 
-    db.session.add(new_departament)
+    db.session.add(new_department)
     db.session.commit()
 
     return (
-        jsonify({"id": new_departament.id, "name": new_departament.name}),
+        jsonify({"id": new_department.id, "name": new_department.name}),
         201,
-    )  # 201 - Created
+    )
 
 
 def user_detail_response(user):
@@ -162,9 +169,9 @@ def user_detail_response(user):
         "name": user.name,
         "second_name": user.second_name,
         "email": user.email,
-        "departament": {
-            "id": user.departament.id,
-            "name": user.departament.name,
+        "department": {
+            "id": user.department.id,
+            "name": user.department.name,
         },
         "date_time_creation": user.date_time_creation.strftime("%Y-%m-%d %H:%M:%S"),
         "date_time_updated": (
--- models.py
+++ models.py
@@ -8,19 +8,19 @@ import math
 db = SQLAlchemy()
 
 
-class Departament(db.Model):
-    __tablename__ = "departament"
+class Department(db.Model):
+    __tablename__ = "department"
     id = db.Column(db.Integer, primary_key=True)
     name = db.Column(db.String(100))
 
-    # initial values into the departments table
+    # Running an initial load with random departments
     @staticmethod
     def insert_initial_values():
-        departaments = ["DESENVOLVIMENTO", "QUALIDADE", "MARKETING", "ADMINISTRATIVO"]
-        for departament in departaments:
-            if not Departament.query.filter_by(name=departament).first():
-                new_departament = Departament(name=departament)
-                db.session.add(new_departament)
+        departments = ["DESENVOLVIMENTO", "QUALIDADE", "MARKETING", "ADMINISTRATIVO"]
+        for department in departments:
+            if not Department.query.filter_by(name=department).first():
+                new_department = Department(name=department)
+                db.session.add(new_department)
         db.session.commit()
 
 
@@ -30,12 +30,12 @@ class User(db.Model):
     name = db.Column(db.String(100))
     second_name = db.Column(db.String(100))
     email = db.Column(db.String(100))
-    departament_id = db.Column(db.Integer, db.ForeignKey("departament.id"))
-    departament = db.relationship("Departament", backref=db.backref("users", lazy=True))
+    department_id = db.Column(db.Integer, db.ForeignKey("department.id"))
+    department = db.relationship("Department", backref=db.backref("users", lazy=True))
     date_time_creation = db.Column(db.DateTime)
     date_time_updated = db.Column(db.DateTime)
 
-    # initial values into the users table
+    # Running an initial load with random user data
     @staticmethod
     def insert_initial_values():
         users_name = [
@@ -71,41 +71,57 @@ class User(db.Model):
                 + "_"
                 + "".join(random.choice(string.digits) for i in range(3))
                 + "@email.com",
-                departament_id=random.randint(1, 4),
+                department_id=random.randint(1, 4),
                 date_time_creation=datetime.now(),
             )
             db.session.add(new_user)
         db.session.commit()
 
-    # paginated search results
+    # Executing a query paged by: page number, total records and department name
     @staticmethod
-    def get_paginated_users(page, total_page):
+    def get_paginated_users(page, total_page, department):
         formatted_results = []
-        users = User.query.paginate(page=page, per_page=total_page)
 
-        for user in users.items:
-            departament = Departament.query.filter_by(id=user.departament_id).first()
-            formatted_user = {
-                "id": user.id,
-                "name": user.name,
-                "departament": {
-                    "id": departament.id,
-                    "name": departament.name,
-                },
-                "date_time_creation": user.date_time_creation.strftime(
-                    "%Y-%m-%d %H:%M:%S"
-                ),
-                "date_time_updated": (
-                    user.date_time_updated.strftime("%Y-%m-%d %H:%M:%S")
-                    if user.date_time_updated
-                    else ""
-                ),
-            }
-            formatted_results.append(formatted_user)
+        if department:
+            users_query = User.query.join(Department).filter(
+                Department.name == department
+            )
+        else:
+            users_query = User.query
+
+       
+        total_records = users_query.count()
+        total_pages = math.ceil(total_records / total_page)
+
+        if page < 1 or page > total_pages:
+            return None 
+
+        if total_records > 0:
+            users = users_query.paginate(page=page, per_page=total_page)
+
+            for user in users.items:
+                department = Department.query.filter_by(id=user.department_id).first()
+                formatted_user = {
+                    "id": user.id,
+                    "name": user.name,
+                    "department": {
+                        "id": department.id,
+                        "name": department.name,
+                    },
+                    "date_time_creation": user.date_time_creation.strftime(
+                        "%Y-%m-%d %H:%M:%S"
+                    ),
+                    "date_time_updated": (
+                        user.date_time_updated.strftime("%Y-%m-%d %H:%M:%S")
+                        if user.date_time_updated
+                        else ""
+                    ),
+                }
+                formatted_results.append(formatted_user)
 
         return {
             "page": page,
-            "total_pages": math.ceil(users.total / total_page),
-            "total_records": users.total,
+            "total_pages": total_pages,
+            "total_records": total_records,
             "results": formatted_results,
         }
